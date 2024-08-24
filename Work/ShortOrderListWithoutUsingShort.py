c867aa402a3681303a38bdb2ce7746e54d4047af
AryaN <sunilkumaharana459@gmail.com>
2024-08-24 11:19:34 +0530

    Work bubble short/ swap

---
+++ Work/ShortOrderListWithoutUsingShort.py
@@ -0,0 +1,8 @@
+list=[9,26,7,6,30,56,25]
+
+for i in range(len(list)):
+    for j in range(0, len(list)-i-1):
+        if list[j] > list[j + 1]:
+            list[j], list[j + 1] = list[j + 1], list[j]
+
+print(list)
