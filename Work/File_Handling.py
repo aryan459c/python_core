bb6b109ba09b9c7929218125b749b0fde690a35b
AryaN <sunilkumaharana459@gmail.com>
2024-08-25 01:52:32 +0530

    read write append with CSV JSON

--- Work/File_Handling.py
+++ Work/File_Handling.py
@@ -1,4 +1,6 @@
 """read mode(r)"""
+import json
+
 file=open("demo.txt","r")
 print(file.read())
 print(file.read(5))
@@ -18,3 +20,44 @@ var.write("Water")
 with open("file path","mode") as var,open("fil_path") as var2:
     var.read()
     print(var.closed) #Check File closed or not
+
+
+"""How to handel  csv and excel file"""
+"""How to create csv file & insert data"""
+import  csv
+data_col=["id","name","age"]
+data_cols=[[10,"abc",22],[11,"def",26],[12,"cba",23]]
+with open("output.csv",mode="w") as file:
+    csv_writer=csv.writer(file)
+    csv_writer.writerow(data_col)
+    csv_writer.writerows(data_cols)
+
+
+"""Read CSV data"""
+
+with open("output.csv",mode="r") as file:
+    csv_reader=csv.reader(file)
+    for i in csv_reader:
+        print(i)
+
+
+"""Handel JSON file"""
+"""
+DUMPS=Convert a dict to byte string(Json)
+LOADS= Convert byte Sting to dict
+"""
+
+"""For Read Json"""
+with open("new_data.json",mode="r") as file:
+    json_reader=file.read()
+    json_loads=json.loads(json_reader)
+
+"""Write Json data"""
+json_data={
+    "id":10,
+    "salary":3000,
+    "Age":28
+}
+with open("create_json.json",mode="w") as file:
+    dict_con=json.dumps(json_data)
+    json_writer=file.write(dict_con)
